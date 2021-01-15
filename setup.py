68a0a1585b42df1fb5db16498ceeb833a7a7babb
Sasha Friedenberg <sashafriedenberg@gmail.com>
2021-01-15 18:56:37 -0500

    update package info

--- setup.py
+++ setup.py
@@ -6,26 +6,25 @@ with open('VERSION', 'r') as f:
     version = f.read()
 
 setuptools.setup(
-    name="gummy",
-    version=version,
-    author="Sasha Friedenberg",
-    author_email="carley.f253fa96@icantbelieveitsnotgmail.com",
-    description="""allows copying and pasting specific UTI's from the macOS
-    Pasteboard""",
-    url="https://github.com/friedenberg/gummy",
-    packages=['gummy'],
-    package_dir={'gummy': 'src'},
-    install_requires=[
+    name             = "gummy",
+    version          = version,
+    author           = "Sasha Friedenberg",
+    author_email     = "carley.f253fa96@icantbelieveitsnotgmail.com",
+    description      = """copy specific UTI's from the macOS Pasteboard""",
+    url              = "https://github.com/friedenberg/gummy",
+    packages         = ['gummy'],
+    package_dir      = {'gummy': 'src'},
+    install_requires = [
         'pyobjc-core',
         'pyobjc-framework-Cocoa',
         ],
-    classifiers=[
+    classifiers      = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Environment :: MacOS X :: Cocoa",
     ],
-    python_requires='>=3.7.3',
-    entry_points = {
+    python_requires  = '>=3.7.6',
+    entry_points     = {
         'console_scripts': ['gummy = gummy.__main__:main'],
     }
 )
