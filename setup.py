2c28f4d7c09db9ca2b12891758c80029537a713a
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2023-02-22 09:00:05 -0500

    last time

--- setup.py
+++ setup.py
@@ -11,25 +11,27 @@ with open(file_version) as f:
     version = main_ns['__version__']
 
 setuptools.setup(
-    name             = "tacky",
-    version          = version,
-    author           = "Sasha F",
-    author_email     = "carley.f253fa96@icantbelieveitsnotgmail.com",
-    description      = """copy and paste specific UTI's from the macOS Pasteboard""",
-    url              = "https://github.com/friedenberg/tacky",
-    packages         = ['tacky'],
-    package_dir      = {'tacky': 'src'},
-    install_requires = [
-        'pyobjc-core',
-        'pyobjc-framework-Cocoa',
-        ],
-    classifiers      = [
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Environment :: MacOS X :: Cocoa",
-    ],
-    python_requires  = '>=3.6.8',
-    entry_points     = {
-        'console_scripts': ['tacky = tacky.__main__:main'],
-    }
-)
+        name             = "tacky",
+        version          = version,
+        author           = "Sasha F",
+        author_email     = "carley.f253fa96@icantbelieveitsnotgmail.com",
+        description      = """copy and paste specific UTI's from the macOS Pasteboard""",
+        long_description_content_type = "text/markdown",
+        long_description = """copy and paste specific UTI's from the macOS Pasteboard""",
+        url              = "https://github.com/friedenberg/tacky",
+        packages         = ['tacky'],
+        package_dir      = {'tacky': 'src'},
+        install_requires = [
+            'pyobjc-core',
+            'pyobjc-framework-Cocoa',
+            ],
+        classifiers      = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Environment :: MacOS X :: Cocoa",
+            ],
+        python_requires  = '>=3.6.8',
+        entry_points     = {
+            'console_scripts': ['tacky = tacky.__main__:main'],
+            }
+        )
